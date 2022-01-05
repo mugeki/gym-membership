561404b98f6566e8ba1c3f72d090d96034c8136c
Muhamad Elang Ramadhan <elangmugeki@gmail.com>
2022-01-05 22:04:43 +0700

    fix response

--- controllers/users/response/json.go
+++ controllers/users/response/json.go
@@ -3,7 +3,6 @@ package response
 type Users struct {
 	ID        uint   `json:"id"`
 	Username  string `json:"username"`
-	Password  string `json:"password"`
 	Email     string `json:"email"`
 	FullName  string `json:"fullname"`
 	Gender    string `json:"gender"`
