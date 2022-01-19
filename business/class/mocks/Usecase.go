7bd9079f8031a8b2e13223e88614f2a33c989f7c
amirohqurrota <qurrotaayunamiroh@gmail.com>
2022-01-19 10:59:10 +0700

    mock class domain

--- business/class/mocks/Usecase.go
+++ business/class/mocks/Usecase.go
@@ -57,6 +57,27 @@ func (_m *Usecase) GetAll(title string, classType string, page int) ([]class.Dom
 	return r0, r1, r2, r3, r4
 }
 
+// GetClassByID provides a mock function with given fields: idClass
+func (_m *Usecase) GetClassByID(idClass uint) (class.Domain, error) {
+	ret := _m.Called(idClass)
+
+	var r0 class.Domain
+	if rf, ok := ret.Get(0).(func(uint) class.Domain); ok {
+		r0 = rf(idClass)
+	} else {
+		r0 = ret.Get(0).(class.Domain)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(uint) error); ok {
+		r1 = rf(idClass)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Insert provides a mock function with given fields: classData
 func (_m *Usecase) Insert(classData *class.Domain) (string, error) {
 	ret := _m.Called(classData)
